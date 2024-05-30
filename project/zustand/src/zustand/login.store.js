2112906ac3996d6afa34346be4ef9e9726c50785
butterbeetle <butterbeetle@naver.com>
2024-05-30 10:59:19 +0900

    05. 수업. zustand 마무리

--- project/zustand/src/zustand/login.store.js
+++ project/zustand/src/zustand/login.store.js
@@ -1,15 +1,29 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-const useLoginStore = create((set) => ({
-  isLoggedIn: false,
-  logIn: () =>
-    set(() => ({
-      isLoggedIn: true,
-    })),
-  logOut: () =>
-    set(() => ({
+const useLoginStore = create(
+  persist(
+    (set, get) => ({
       isLoggedIn: false,
-    })),
-}));
+      logIn: () =>
+        set(() => ({
+          isLoggedIn: true,
+        })),
+      logOut: () =>
+        set(() => ({
+          isLoggedIn: false,
+        })),
+      // toggleIsLoggedIn:()=>set((prevState)=>({isLoggedIn:!prevState.isLoggedIn}))
+      toggleIsLoggedIn: () => {
+        const prevState = get();
+        set({ isLoggedIn: !prevState.isLoggedIn });
+      },
+    }),
+    {
+      name: "login-store",
+      storage: createJSONStorage(() => localStorage), // 이 줄 안쓰면 local storage 기본, session에 하고싶으면 sessionStorage로 적음
+    }
+  )
+);
 
 export default useLoginStore;
